90acf5cfc49fa97adf1cc8707d2b3b42109f4649
nickroberts404 <nickroberts404@gmail.com>
2019-04-10 22:55:29 -0500

    Initial sequencer component and d3 hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import { select, pie, arc, mouse, event } from 'd3';
+const width = 500;
+const height = 500;
+const getInitialSequence = (layers, steps) => {
+	const sequence = [];
+	for (var i = 0; i < layers; i++) {
+		const layer = [];
+		for (var j = 0; j < steps; j++) {
+			layer.push(Math.floor(Math.random() * 127));
+		}
+		sequence.push(layer);
+	}
+	return sequence;
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const initialSequence = getInitialSequence(4, 16);
+
+const App = () => {
+	return <Vis />;
+};
+
+const Vis = () => {
+	const [ref, setRef] = useState(null);
+	const [sequence, setSequence] = useState(initialSequence);
+	useRing(ref, 100, 250, sequence[0]);
+	return <svg ref={setRef} height={height} width={width} />;
+};
+
+const useRing = (node, inner, outer, data) => {
+	const arcs = pie().value(1)(data);
+	const getArc = arc()
+		.innerRadius(inner)
+		.outerRadius(outer);
+	const handleClick = (a, b, c) => {
+		console.log(event);
+	};
+	useEffect(() => {
+		if (node) {
+			const g2 = select(node)
+				.append('g')
+				.attr('transform', `translate(${width / 2},${height / 2})`);
+			g2.selectAll('path')
+				.data(arcs)
+				.enter()
+				.append('path')
+				.attr('fill', 'blue')
+				.attr('d', (d) => {
+					const getA = arc()
+						.innerRadius(inner)
+						.outerRadius(inner + ((outer - inner) / 127) * d.data);
+					return getA(d);
+				});
+			const g = select(node)
+				.append('g')
+				.attr('transform', `translate(${width / 2},${height / 2})`);
+			g.selectAll('path')
+				.data(arcs)
+				.enter()
+				.append('path')
+				.attr('fill', 'transparent')
+				.attr('stroke', '#eee')
+				.attr('d', getArc)
+				.on('click', handleClick);
+		}
+	}, [node, data]);
+};
 
 export default App;
